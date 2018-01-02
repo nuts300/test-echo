5fa7e5ec0415fa69615fe7077a1cfb152b48357b
nuts <nuts300@gmail.com>
2018-01-02 17:57:30 +0900

    Initial commit

---
+++ controllers/users.go
@@ -0,0 +1,12 @@
+package controllers
+
+import (
+	"net/http"
+
+	"github.com/labstack/echo"
+)
+
+func getUser(c echo.Context) (err error) {
+	id := c.Param("id")
+	return c.String(http.StatusOK, id)
+}
---
+++ main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/labstack/echo"
+)
+
+func main() {
+	e := echo.New()
+	e.GET("/", func(c echo.Context) error {
+		return c.String(http.StatusOK, "Hello, World!")
+	})
+
+	// e.POST("/users", saveUser)
+	e.GET("/users/:id", getUser)
+	// e.PUT("/users/:id", updateUser)
+	// e.DELETE("/users/:id", deleteUser)
+
+	e.Logger.Fatal(e.Start(":1323"))
+}
