3fe8347748a71219193c37061fa3e45a6f9ae58c
nuts <nuts300@gmail.com>
2018-01-04 18:14:13 +0900

    Add cors middleware

--- main.go
+++ main.go
@@ -17,6 +17,10 @@ func main() {
 
 	e.Use(middleware.Logger())
 	e.Use(middleware.Recover())
+	e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
+		AllowOrigins: []string{"*"},
+		AllowMethods: []string{echo.GET, echo.PUT, echo.POST, echo.DELETE},
+	}))
 
 	hello := helloController.New()
 
