bdad46d02428a977d8a76d0c7c178c4147b5455c
nuts <nuts300@gmail.com>
2018-01-05 20:18:26 +0900

    Encrypt password

--- models/user.go
+++ models/user.go
@@ -1,9 +1,22 @@
 package models
 
+import (
+	"github.com/jinzhu/gorm"
+	"golang.org/x/crypto/bcrypt"
+)
+
 type User struct {
 	Email    string `json:"email" yaml:"email" validate:"required,email"`
 	Password string `json:"password" yaml:"password" validate:"required"`
 	Base
+}
+
+func (user *User) BeforeSave(scope *gorm.Scope) (err error) {
+	if pw, err := bcrypt.GenerateFromPassword([]byte(user.Password), 0); err == nil {
+		scope.SetColumn("password", pw)
+		return err
+	}
+	return nil
 }
 
 func NewUser() User {
